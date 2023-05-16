4543e80430b43174e0007ebcc9c05152fa723150
Vando Dos Reis <vandodosreis2001@gmail.com>
2023-05-16 06:31:51 -0300

    refactor: conditional match

--- public_html/index.php
+++ public_html/index.php
@@ -2,26 +2,31 @@
     header('Content-Type: application/json');
     require_once '../vendor/autoload.php';
 
+    function api($url): void
+    {
+        array_shift($url);
+        $service = "App\Services\\".ucfirst($url[0])."Service";
+        array_shift($url);
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+
+        try {
+            $response = call_user_func_array([new $service, $method], $url);
+            http_response_code(200);
+            echo json_encode(['status' => 'success', 'data' => $response]);
+            exit;
+        } catch (\Exception $error) {
+            http_response_code(404);
+            echo json_encode(
+              array('status' => 'error', 'data' => $error->getMessage()),
+              JSON_UNESCAPED_UNICODE
+            );
+            exit;
+        }
+    }
+
     if ($_GET['url']) {
         $url = explode('/', $_GET['url']);
-        if ($url[0] === 'api') {
-            array_shift($url);
-            $service = "App\Services\\".ucfirst($url[0])."Service";
-            array_shift($url);
-            $method = strtolower($_SERVER['REQUEST_METHOD']);
-
-            try {
-                $response = call_user_func_array([new $service, $method], $url);
-                http_response_code(200);
-                echo json_encode(['status' => 'success', 'data' => $response]);
-                exit;
-            } catch (\Exception $error) {
-                http_response_code(404);
-                echo json_encode(
-                  array('status' => 'error', 'data' => $error->getMessage()),
-                  JSON_UNESCAPED_UNICODE
-                );
-                exit;
-            }
-        }
+        match($url[0]) {
+            'api' => api($url),
+        };
     }
